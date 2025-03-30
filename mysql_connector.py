b8af7cf922052d7d8982c9a5991570faf5110ed1
adarshrajamanickam <arajam8419@gmail.com>
2025-03-29 20:14:51 -0400

    adding values

--- mysql_connector.py
+++ mysql_connector.py
@@ -1,6 +1,8 @@
 import mysql.connector
 import os
 from dotenv import load_dotenv
+from flask import Flask, render_template, request, redirect, url_for
+
 
 load_dotenv()
 key = os.getenv("MYSQL_PASSWORD") # create a .env file with your MySQL password
@@ -17,3 +19,28 @@ mycursor = mydb.cursor()
 mycursor.execute("SHOW TABLES")
 for table in mycursor:
     print(table)
+
+#flask instance
+app = Flask(__name__)
+
+config = {
+    'host' : 'localhost',
+    'user' : 'root',
+    'password' : key, 
+    'database' : 'mydb'
+    
+}
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/add',methods=['POST'])
+def add():
+    data = request.form['data']
+    conn = mysql.connector.connect(**config)
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO employee (employee_id) VALUES (%d)", (data,))
+    conn.commit()
+    conn.close()
+    return redirect(url_for('index'))
