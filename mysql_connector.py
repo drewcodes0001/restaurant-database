be07ee19f8324fc80af9cd5474c6ab2f564958d4
BigTruzz8 <103378515+BigTruzz8@users.noreply.github.com>
2025-04-11 15:50:29 -0400

    upadate .py backend file for shifts
    upadate .py backend file for shifts

---
+++ mysql_connector.py
@@ -0,0 +1,114 @@
+import mysql.connector
+import os
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask_cors import CORS
+
+
+
+
+load_dotenv()
+key = os.getenv("MYSQL_PASSWORD") # create a .env file with your MySQL password
+
+mydb = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    password = key, 
+    port = '3306',
+    database = 'mydb' 
+)
+
+mycursor = mydb.cursor() 
+mycursor.execute("SHOW TABLES")
+for table in mycursor:
+    print(table)
+
+#flask instance
+app = Flask(__name__)
+CORS(app)
+
+config = {
+    'host' : 'localhost',
+    'user' : 'root',
+    'password' : key, 
+    'database' : 'mydb'
+    
+}
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+@app.route('/add', methods=['POST'])
+def add():
+    data = request.get_json()  # Receive JSON data
+    if not data:
+        return jsonify({"error": "No data provided"}), 400
+      
+    id = data.get('id')
+    username = data.get('username')
+    password = data.get('password')
+    fullname = data.get('fullname')
+    wage = data.get('wage')
+    
+    conn = mysql.connector.connect(**config)
+    cursor = conn.cursor()
+    cursor.execute(
+        "INSERT INTO employee (employee_id, username, password, Full_name, wage) VALUES (%s, %s, %s, %s, %s)",
+        (id, username, password, fullname, wage)
+    )
+    conn.commit()
+    conn.close()
+    return jsonify({"message": "Employee added successfully"}), 200
+
+@app.route('/delete', methods=['POST'])
+def delete():
+    data = request.get_json()  # Receive JSON data
+    if not data:
+        return jsonify({"error": "No data provided"}), 400
+      
+    id = data.get('id_remove')
+    
+    conn = mysql.connector.connect(**config)
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM employee WHERE employee_id = (%s)", (id,))
+    conn.commit()
+    conn.close()
+    return jsonify({"message": "Employee removed successfully"}), 200
+
+@app.route('/add_shift', methods=['POST'])
+def add_shift():
+    data = request.get_json()
+
+    if not data:
+        return jsonify({"error": "No data provided"}), 400
+
+    shift_id = data.get('shift_id')
+    date = data.get('date')
+    duration = data.get('duration')
+    
+    
+    conn = mysql.connector.connect(**config)
+    cursor = conn.cursor()
+    cursor.execute(
+        "INSERT INTO Shift (Shift_id, Date, Duration) VALUES (%s, %s, %s)",
+        (shift_id, date, duration)
+    )
+    conn.commit()
+    conn.close()
+    return jsonify({"message": "Shift added successfully"}), 200
+    
+@app.route('/delete_shift', methods=['POST'])
+def delete_shift():
+    data = request.get_json()  # Receive JSON data
+    if not data:
+        return jsonify({"error": "No data provided"}), 400
+      
+    shift_id = data.get('id_remove_shift')
+    
+    conn = mysql.connector.connect(**config)
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM Shift WHERE Shift_ID = (%s)", (shift_id,))
+    conn.commit()
+    conn.close()
+    return jsonify({"message": "Shift removed successfully"}), 200
