06b07a64eebf143769c696246ab5724c0fffdcf8
drewcodes0001 <andrew.tang367@gmail.com>
2025-04-11 20:03:32 -0400

    Updated to include add and delete functionality

--- mysql_connector.py
+++ mysql_connector.py
@@ -27,5 +27,33 @@ mycursor.execute("SHOW TABLES")
 for table in mycursor:
     print(table)
 
-mycursor.close();
+mycursor.close()
+
+@app.route('/add', methods=['POST'])
+def add():
+    id = request.form['id']
+    username = request.form['username']
+    password = request.form['password']
+    fullname = request.form['fullname']
+    wage = request.form['wage']
+    # Optional: handle missing start_date and supervisor_username
+    conn = mysql.connector.connect(**config)
+    cursor = conn.cursor()
+    cursor.execute("""
+        INSERT INTO employees (employee_id, username, password, full_name, wage)
+        VALUES (%s, %s, %s, %s, %s)
+    """, (id, username, password, fullname, wage))
+    conn.commit()
+    conn.close()
+    return redirect(url_for('index'))
+
+@app.route('/delete', methods=['POST'])
+def delete():
+    id = request.form['id_remove']
+    conn = mysql.connector.connect(**config)
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM employees WHERE employee_id = %s", (id,))
+    conn.commit()
+    conn.close()
+    return redirect(url_for('index'))
 
